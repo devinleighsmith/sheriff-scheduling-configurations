05cf2a4687865b6ea699808b1c1583f711f8a09e
Devin Smith <devinleighsmith@gmail.com>
2020-09-18 08:11:00 -0700

    Initial commit of dc/bc/pipeline based on supreme court viewer config.

---
+++ jenkins/build/config.groovy
@@ -0,0 +1,8 @@
+// Pipeline Configuration Properties
+// Import this file into the pipeline using 'load'.
+class config extends bc.baseConfig {
+  // Build configuration
+  public static final String[] BUILDS = ["web-runtime", "web-artifacts", "web", "ss-api"]
+}
+
+return new config();
---
+++ openshift/settings.sh
@@ -0,0 +1,3 @@
+export PROJECT_NAMESPACE="jag-shuber"
+export GIT_URI="https://github.com/bcgov/sheriff-scheduling.git"
+export GIT_REF="master"
