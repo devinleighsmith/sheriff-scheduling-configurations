f69a39063616982ab83fc43a24888f21a306b68b
Wade Barnes <wade.barnes@shaw.ca>
2021-02-27 13:39:29 -0800

    Update configurations for network security policies.
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

--- openshift/templates/api/api-deploy.overrides.sh
+++ openshift/templates/api/api-deploy.overrides.sh
@@ -20,19 +20,22 @@ if createOperation; then
   writeParameter "DATA_PROTECTION_ENCRYPTION_KEY" $(generateKey 32 | fold -w 32 | head -n 1) "false"
 
   # Get Location Services settings
-  readParameter "LOCATION_SERVICES_CLIENT_URL - Please provide the endpoint (URL) for the Location Services API." LOCATION_SERVICES_CLIENT_URL "" "false" 
-  readParameter "LOCATION_SERVICES_CLIENT_USERNAME - Please provide the username to use with the Location Services API." LOCATION_SERVICES_CLIENT_USERNAME "" "false" 
-  readParameter "LOCATION_SERVICES_CLIENT_PASSWORD - Please provide the password to use with the Location Services API." LOCATION_SERVICES_CLIENT_PASSWORD "" "false" 
+  readParameter "LOCATION_SERVICES_CLIENT_URL - Please provide the endpoint (URL) for the Location Services API." LOCATION_SERVICES_CLIENT_URL "" "false"
+  parseHostnameParameter "LOCATION_SERVICES_CLIENT_URL" "LOCATION_SERVICES_CLIENT_HOST"
+  readParameter "LOCATION_SERVICES_CLIENT_USERNAME - Please provide the username to use with the Location Services API." LOCATION_SERVICES_CLIENT_USERNAME "" "false"
+  readParameter "LOCATION_SERVICES_CLIENT_PASSWORD - Please provide the password to use with the Location Services API." LOCATION_SERVICES_CLIENT_PASSWORD "" "false"
 
   # Get KeyCloak settings
-  readParameter "KEYCLOAK_AUTHORITY - Please provide the endpoint (URL) for the OIDC relaying party." KEYCLOAK_AUTHORITY "" "false" 
+  readParameter "KEYCLOAK_AUTHORITY - Please provide the endpoint (URL) for the OIDC relaying party." KEYCLOAK_AUTHORITY "" "false"
   parseHostnameParameter "KEYCLOAK_AUTHORITY" "OIDC_RP_HOST"
-  readParameter "KEYCLOAK_SECRET - Please provide the API secret toi use with the OIDC relaying party." KEYCLOAK_SECRET "" "false" 
-  readParameter "SITEMINDER_LOGOUT_URL - Please provide the SiteMinder Logout URL." SITEMINDER_LOGOUT_URL "" "false" 
+  readParameter "KEYCLOAK_SECRET - Please provide the API secret toi use with the OIDC relaying party." KEYCLOAK_SECRET "" "false"
+  readParameter "SITEMINDER_LOGOUT_URL - Please provide the SiteMinder Logout URL." SITEMINDER_LOGOUT_URL "" "false"
 
   # Get the email settings
-  readParameter "EMAIL_SERVICE_URL - Please provide the url for the CHES email api.  The default is a blank string." EMAIL_SERVICE_URL "" "false"  
+  readParameter "EMAIL_SERVICE_URL - Please provide the url for the CHES email api.  The default is a blank string." EMAIL_SERVICE_URL "" "false"
+  parseHostnameParameter "EMAIL_SERVICE_URL" "EMAIL_SERVICE_HOST"
   readParameter "EMAIL_SERVICE_AUTH_URL - Please provide the url for the CHES authentication endpoint.  The default is a blank string." EMAIL_SERVICE_AUTH_URL "" "false"
+  parseHostnameParameter "EMAIL_SERVICE_AUTH_URL" "EMAIL_SERVICE_AUTH_HOST"
   readParameter "EMAIL_SERVICE_CLIENT_ID - Please provide the service client id for sending access request emails.  The default is a blank string." EMAIL_SERVICE_CLIENT_ID "" "false"
   readParameter "EMAIL_SERVICE_CLIENT_SECRET - Please provide the service client secret to use with above id.  The default is a blank string." EMAIL_SERVICE_CLIENT_SECRET "" "false"
   readParameter "SENDER_EMAIL - Please provide the email address used for sending access request emails.  The default is a blank string." SENDER_EMAIL "" "false"
@@ -46,7 +49,7 @@ else
   writeParameter "LOCATION_SERVICES_CLIENT_URL" "prompt_skipped" "false"
   writeParameter "LOCATION_SERVICES_CLIENT_USERNAME" "prompt_skipped" "false"
   writeParameter "LOCATION_SERVICES_CLIENT_PASSWORD" "prompt_skipped" "false"
-  
+
   writeParameter "KEYCLOAK_AUTHORITY" "prompt_skipped" "false"
   writeParameter "KEYCLOAK_SECRET" "prompt_skipped" "false"
   writeParameter "SITEMINDER_LOGOUT_URL" "prompt_skipped" "false"
@@ -62,6 +65,18 @@ else
   # Get OIDC_RP_HOST from secret
   printStatusMsg "Getting OIDC_RP_HOST for the ExternalNetwork definition from secret ...\n"
   writeParameter "OIDC_RP_HOST" $(getSecret "${NAME}" "oidc-rp-host") "false"
+
+  # Get LOCATION_SERVICES_CLIENT_HOST from secret
+  printStatusMsg "Getting LOCATION_SERVICES_CLIENT_HOST for the ExternalNetwork definition from secret ...\n"
+  writeParameter "LOCATION_SERVICES_CLIENT_HOST" $(getSecret "${NAME}" "location-services-client-host") "false"
+
+  # Get EMAIL_SERVICE_HOST from secret
+  printStatusMsg "Getting EMAIL_SERVICE_HOST for the ExternalNetwork definition from secret ...\n"
+  writeParameter "EMAIL_SERVICE_HOST" $(getSecret "${NAME}" "email-service-host") "false"
+
+  # Get EMAIL_SERVICE_AUTH_HOST from secret
+  printStatusMsg "Getting EMAIL_SERVICE_AUTH_HOST for the ExternalNetwork definition from secret ...\n"
+  writeParameter "EMAIL_SERVICE_AUTH_HOST" $(getSecret "${NAME}" "email-service-auth-host") "false"
 fi
 
 SPECIALDEPLOYPARMS="--param-file=${_overrideParamFile}"
